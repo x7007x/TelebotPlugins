930cfaccd9709f94efb06b744d6a4569c6525242
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2025-03-09 05:13:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="telebot-plugins",
-    version="0.1.2",
+    version="0.1.3",
     author="Ahmed Negm",
     author_email="A7medNegm.x@gmail.com",
     description="A simple plugin system for pyTelegramBotAPI (telebot)",
